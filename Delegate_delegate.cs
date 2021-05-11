b6e77a56f0ce6b89de81516252b07a36c7111791
ZhongWen <ZhongWen@DESKTOP-LFKTQNR>
2021-05-11 08:55:48 +0800

    添加项目文件。

---
+++ Delegate_delegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSStudy
+{
+    //声明一个委托
+    delegate string delegateIntToString(int i);
+    class Delegate_delegate
+    {
+       //创建一个和委托匹配的方法
+       public string FIntToString(int i)
+        {
+            return i.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSStudy
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Delegate_delegate
+            //创建Delegate_delegate实列
+            Delegate_delegate delegate_Delegate = new Delegate_delegate();
+            //实列一个委托,把Delegate_delegate实列的FIntToString方法作为参数
+            delegateIntToString delegateIntToString = new delegateIntToString(delegate_Delegate.FIntToString);
+            //直接输出委托
+            Console.WriteLine(delegateIntToString(45645321)+ "这是Delegate_delegate的结果");
+            #endregion
+        }
+    }
+}
